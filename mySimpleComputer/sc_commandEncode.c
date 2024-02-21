7b40178f371ae8bc544975a188023961cb6ae153
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-02-21 21:07:18 +0700

    Add sc_commandDecode

--- mySimpleComputer/sc_commandEncode.c
+++ mySimpleComputer/sc_commandEncode.c
@@ -3,7 +3,7 @@
 
 int sc_commandEncode(int sign, int command, int operand, int * value)
 {
-    if (!value || !(sign == 1 || sign == 0) || !(command >= 0 && command <= 0x07ff) || !(operand >= 0 && operand <= 0x07ff))
+    if (!value || !(sign == 1 || sign == 0) || !(command >= 0 && command <= 0x7f) || !(operand >= 0 && operand <= 0x7f))
     {
         return -1;
     }
@@ -13,14 +13,35 @@ int sc_commandEncode(int sign, int command, int operand, int * value)
     return 0;
 }
 
+int sc_commandDecode(int value, int * sign, int * command, int * operand)
+{
+    if (!sign || !command || !operand)
+    {
+        return -1;
+    }
+
+    *sign = value >> 15;
+    *command = (value >> 8) & 0x7f;
+    *operand = value & 0x7f;
+
+    return 0;
+}
+
 int main()
 {
 
     int value = 0;
+    int operand = 0;
+    int command = 0;
+    int sign= 0;
 
-    sc_commandEncode(1, 15, 16, &value);
+    sc_commandEncode(0, 120, 67, &value);
 
     printf("%d\n", value);
+
+    sc_commandDecode(value, &sign, &command, &operand);
+
+    printf("%d %d %d\n", sign, command, operand);
 
     return 0;
 }
