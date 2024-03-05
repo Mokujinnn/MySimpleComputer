6c2ea9111f9c7d68c18cd641c7105843fb4d98ae
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-03-05 20:35:53 +0700

    Add printCommand

--- console/printCommand.c
+++ console/printCommand.c
@@ -1,6 +1,16 @@
 #include <unistd.h>
 
+#include "mySimpleComputer.h"
+
 void printCommand ()
 {
-    
+    int value = 0;
+    int sign = 0;
+    int command = 0;
+    int operand = 0;
+
+    sc_icounterGet(&value);
+    sc_commandDecode(value, &sign, &command, &operand);
+
+
 }
