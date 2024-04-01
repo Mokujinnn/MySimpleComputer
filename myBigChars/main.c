36c223f83c2b41b22f5ca69c9fce970d0741dc16
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-04-01 16:14:33 +0700

    Add Makefile

---
+++ myBigChars/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "myTerm.h"
+
+int bc_strlen(char * str);
+int bc_box(int x1, int y1, int x2, int y2, enum colors box_fg,
+enum colors box_bg, char *header, enum colors header_fg, enum colors header_bg);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
