49aae7ca8837b2cd4bba5d4a8d72ea3e01e36acd
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-04-02 15:10:51 +0700

    Add bc_box

--- myBigChars/bc_box.c
+++ myBigChars/bc_box.c
@@ -1,20 +1,72 @@
 #include <stdio.h>
+
 #include "myTerm.h"
+#include "myBigChars.h"
 
 int bc_box(int x1, int y1, int x2, int y2, enum colors box_fg,
 enum colors box_bg, char *header, enum colors header_fg, enum colors header_bg)
 {
     int rows = 0;
     int cols = 0;
+    int i = 0;
+    int j = 0;
+
     mt_getscreensize(&rows, &cols);
 
-    if (!(rows >= y2 && cols >= x2))
+    if (y2 > rows || x2 > cols)
     {
         return -1;
     }
     
-    // char tmp[x2-x1] = {0};
-    
+    mt_gotoXY(x1, y1);
+    bc_printA(CHAR_ULC);
 
-    printf("%d %d\n", cols, rows);
+    i = x1 + 1;
+    j = y1;
+    while (i != x1 || j != y1)
+    {
+        if (i < x2 && j == y1)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_HOR_LINE);
+            i++;
+        }
+        else if (i == x2 && j == y1)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_URC);
+            j++;
+        }
+        else if (i == x2 && j < y2)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_VER_LINE);
+            j++;
+        }
+        else if (i == x2 && j == y2)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_LRC);
+            i--;
+        }
+        else if (i > x1 && j == y2)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_HOR_LINE);
+            i--;
+        }
+        else if (i == x1 && j == y2)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_LLC);
+            j--;
+        }
+        else if (i == x1 && j > y1)
+        {
+            mt_gotoXY(i, j);
+            bc_printA(CHAR_VER_LINE);
+            j--;
+        }
+    }
+    
 }
--- myBigChars/main.c
+++ myBigChars/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "myTerm.h"
-
-int bc_strlen(char * str);
-int bc_box(int x1, int y1, int x2, int y2, enum colors box_fg,
-enum colors box_bg, char *header, enum colors header_fg, enum colors header_bg);
+#include "myBigChars.h"
 
 int main(int argc, char const *argv[])
 {
-    
+    mt_clrscr();
+    bc_box(2, 2, 10, 10, ForegroundBlack, ForegroundBlack, "asd", ForegroundBlack, ForegroundBlack);
+    mt_gotoXY(15, 15);
     return 0;
 }
