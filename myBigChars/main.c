3a0368437fe9849adc88f63f2a5740279a3d8ac6
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-04-02 17:40:03 +0700

    Add bc_printbigchar

--- myBigChars/main.c
+++ myBigChars/main.c
@@ -5,7 +5,11 @@
 int main(int argc, char const *argv[])
 {
     mt_clrscr();
-    bc_box(1, 1, 17, 10, ForegroundDarkCyan, BackgroundDefault, "allllll", ForegroundDarkRed, BackgroundBlack);
+    // bc_box(5, 5, 17, 10, ForegroundDarkCyan, BackgroundDefault, "allllll", ForegroundDarkRed, BackgroundBlack);
+    int bigchar1[2] = {0xff0303ff, 0xffc3c3ff};
+    bc_printbigchar(bigchar1, 2, 2, ForegroundDarkRed, BackgroundDefault);
+    int bigchar2[2] = {0xffc3c3ff, 0xffc0c0ff};
+    bc_printbigchar(bigchar2, 11, 2, ForegroundDarkGreen, BackgroundDefault);
     mt_gotoXY(15, 15);
     return 0;
 }
--- myBigChars/myBigChars.h
+++ myBigChars/myBigChars.h
@@ -1,7 +1,7 @@
 #ifndef MY_BIG_CHARS_H
 #define MY_BIG_CHARS_H
 
-#define CHAR_BOX 'a'
+#define CHAR_BOX "a"
 #define CHAR_ULC "l"
 #define CHAR_URC "k"
 #define CHAR_LLC "m"
@@ -9,9 +9,12 @@
 #define CHAR_HOR_LINE "q"
 #define CHAR_VER_LINE "x"
 
+#define BITS_IN_BYTE 8
+#define ARR_SIZE 2
+
 int bc_strlen(char * str);
-int bc_box(int x1, int y1, int x2, int y2, enum colors box_fg,
-enum colors box_bg, char *header, enum colors header_fg, enum colors header_bg);
+int bc_box(int x1, int y1, int x2, int y2, enum colors box_fg, enum colors box_bg, char *header, enum colors header_fg, enum colors header_bg);
 int bc_printA(char * str);
+int bc_printbigchar (int bigchar[ARR_SIZE], int x, int y, enum colors fg, enum colors bg);
 
 #endif //MY_BIG_CHARS_H
