3a3ba2da4b12a5ae4c9eb4b55b5869462f3c0796
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-04-13 18:53:56 +0700

    Add rk_readkey

---
+++ myReadKey/keys.h
@@ -0,0 +1,58 @@
+#ifndef KEYS_H
+#define KEYS_H
+
+enum keys
+{
+    // Functionals keys
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+    ESC,
+
+    //Regular Keys
+    A,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H,
+    I,
+    J,
+    K,
+    L,
+    M,
+    N,
+    O,
+    P,
+    Q,
+    R,
+    S,
+    T,
+    U,
+    V,
+    W,
+    X,
+    Y,
+    Z,
+
+    // Arrows
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
+
+    UNKNOWN
+};
+
+#endif // KEYS_H
---
+++ myReadKey/rk_readkey.c
@@ -0,0 +1,67 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "keys.h"
+
+int rk_readkey(enum keys *key)
+{
+    char buf[7];
+    int r;
+    r = read(STDIN_FILENO, buf, 6);
+    if (r == -1)
+    {
+        return -1;
+    }
+
+    buf[6] = '\0';
+
+    if (strncmp(buf, "l", 1) == 0)
+        *key = L;
+    else if (strncmp(buf, "s", 1) == 0)
+        *key = S;
+    else if (strncmp(buf, "r", 1) == 0)
+        *key = R;
+    else if (strncmp(buf, "t", 1) == 0)
+        *key = T;
+    else if (strncmp(buf, "i", 1) == 0)
+        *key = I;
+    else if (strncmp(buf, "v", 1) == 0)
+        *key = V;
+    else if (strncmp(buf, "p", 1) == 0)
+        *key = P;
+    else if (strncmp(buf, "q", 1) == 0)
+        *key = Q;
+    else if (strncmp(buf, "\E[15~", 5) == 0)
+        *key = F5;
+    else if (strncmp(buf, "\E[17~", 5) == 0)
+        *key = F6;
+    else if (strncmp(buf, "\E[A", 3) == 0)
+        *key = UP;    
+    else if (strncmp(buf, "\E[B", 3) == 0)
+        *key = DOWN;
+    else if (strncmp(buf, "\E[C", 3) == 0)
+        *key = RIGHT;    
+    else if (strncmp(buf, "\E[D", 3) == 0)
+        *key = LEFT;
+    // else if (strncmp(buf, "^[", 1) == 0)
+    //     *key = ESC;    
+    else
+        *key = UNKNOWN;
+
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    enum keys key;
+    while (1)
+    {
+        rk_readkey(&key);
+
+        printf("%d\n", key);
+    }
+
+    return 0;
+}
