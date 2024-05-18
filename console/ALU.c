858bb2babf7636f95ff5f9e7aa280a70e0cb69ef
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-05-18 21:24:58 +0700

    Add ALU

---
+++ console/ALU.c
@@ -0,0 +1,123 @@
+#include "console.h"
+#include "mySimpleComputer.h"
+
+void ALU_ADD(int cell)
+{
+    int accumulator = 0;
+    int memory = 0;
+
+    sc_accumulatorGet(&accumulator);
+    sc_memoryGet(cell, &memory);
+
+    sc_accumulatorSet((accumulator + memory) & 0x7fff);
+}
+
+void ALU_SUB(int cell)
+{
+    int accumulator = 0;
+    int memory = 0;
+
+    sc_accumulatorGet(&accumulator);
+    sc_memoryGet(cell, &memory);
+
+    sc_accumulatorSet((accumulator + (~memory + 1)) & 0x7fff);
+}
+
+void ALU_DIVIDE(int cell)
+{
+    int accumulator = 0;
+    int memory = 0;
+
+    sc_accumulatorGet(&accumulator);
+    sc_memoryGet(cell, &memory);
+
+    if (memory == 0)
+    {
+        sc_regSet(DIVISION_BY_ZERO, 1);
+        sc_regSet(IGNORE_INTERRUPT, 1);
+        return accumulator;
+    }
+
+    int accumulator_sign = accumulator >> 14;
+    int memory_sign = memory >> 14;
+    int value = (accumulator / memory) & 0x3fff;
+    if (accumulator_sign ^ memory_sign)
+    {
+        value = value | 0x4000;
+    }
+
+    sc_accumulatorSet(value);
+}
+
+void ALU_MUL(int cell)
+{
+    int accumulator = 0;
+    int memory = 0;
+
+    sc_accumulatorGet(&accumulator);
+    sc_memoryGet(cell, &memory);
+
+    int accumulator_sign = accumulator >> 14;
+    int memory_sign = memory >> 14;
+
+    if (accumulator_sign)
+    {
+        accumulator = ~accumulator + 1;
+    }
+    if (memory_sign)
+    {
+        memory = ~memory + 1;
+    }
+
+    int value = (memory * accumulator) & 0x3fff;
+
+    if (accumulator_sign ^ memory_sign)
+    {
+        value = (~value + 1) | 0x4000;
+    }
+
+    sc_accumulatorSet(value);
+}
+
+void ALU_NOT(int cell)
+{
+    int value = 0;
+    sc_accumulatorGet(&value);
+    sc_memorySet(cell, ~value);
+}
+
+void ALU_AND(int cell)
+{
+    int value = 0;
+    int mem = 0;
+    sc_accumulatorGet(&value);
+    sc_memoryGet(cell, &mem);
+    sc_accumulatorSet(mem & value);
+}
+
+void ALU(int command, int operand)
+{
+    switch (command)
+    {
+    case ADD:
+        ALU_ADD(operand);
+        break;
+    case SUB:
+        ALU_SUB(operand);
+        break;
+    case DIVIDE:
+        ALU_DIVIDE(operand);
+        break;
+    case MUL:
+        ALU_MUL(operand);
+        break;
+    case NOT:
+        ALU_NOT(operand);
+        break;
+    case AND:
+        ALU_AND(operand);
+        break;
+    default:
+        break;
+    }
+}
--- include/console.h
+++ include/console.h
@@ -3,6 +3,51 @@
 
 #include "myBigChars.h"
 #include "myTerm.h"
+#include <inttypes.h>
+
+enum commands
+{
+  NOP = 0x00,
+  CPUINFO = 0x01,
+  READ = 0x0A,
+  WRITE = 0x0B,
+  LOAD = 0x14,
+  STORE = 0x15,
+  ADD = 0x1E,
+  SUB = 0x1F,
+  DIVIDE = 0x20,
+  MUL = 0x21,
+  JUMP = 0x28,
+  JNEG = 0x29,
+  JZ = 0x2A,
+  HALT = 0x2B,
+  NOT = 0x33,
+  AND = 0x34,
+  OR = 0x35,
+  XOR = 0x36,
+  JNS = 0x37,
+  JC = 0x38,
+  JNC = 0x39,
+  JP = 0x3A,
+  JNP = 0x3B,
+  CHL = 0x3C,
+  SHR = 0x3D,
+  RCL = 0x3E,
+  RCR = 0x3F,
+  NEG = 0x40,
+  ADDC = 0x41,
+  SUBC = 0x42,
+  LOGLC = 0x43,
+  LOGRC = 0x44,
+  RCCL = 0x45,
+  RCCR = 0x46,
+  MOVA = 0x47,
+  MOVR = 0x48,
+  MOVCA = 0x49,
+  MOVCR = 0x4A,
+  ADDC2 = 0x4B,
+  SUBC2 = 0x4C
+};
 
 void printCell (int address, enum colors fg, enum colors bg);
 void printFlags ();
